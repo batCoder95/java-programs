c13106ba6bc7eb68eedc42baf49196416183e627
Vatsal Gosaliya <vatsalgosalia.nmims@gmail.com>
2016-07-17 17:49:56 +0530

    sql-script-commit

---
+++ jdbc-tcp/employeedb/EmployeeDB.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Employee(
+Eid INT NOT NULL,
+name VARCHAR(20) NOT NULL,
+post VARCHAR(20) NOT NULL,
+Did INT NOT NULL,
+salary INT NOT NULL
+);
+
+INSERT INTO Employee (Eid, name, post, Did, salary) VALUES (1, 'ABC', 'Software Engineer', 101, 40000);
+INSERT INTO Employee (Eid, name, post, Did, salary) VALUES (2, 'DEF', 'Systems Engineer', 201, 25000);
+INSERT INTO Employee (Eid, name, post, Did, salary) VALUES (3, 'MNO', 'Data Scientist', 302, 30000);
+INSERT INTO Employee (Eid, name, post, Did, salary) VALUES (4, 'PQR', 'Technical Analyst', 401, 30000);
+INSERT INTO Employee (Eid, name, post, Did, salary) VALUES (5, 'XYZ', 'Java Developer', 501, 50000);
